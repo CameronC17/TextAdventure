b217ea9b9454d9d83b42902bab01e4baaa0a566e
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-15 10:29:19 +0100

    basic js game setup

--- js/app.js
+++ js/app.js
@@ -1,12 +1,47 @@
 window.addEventListener("load", function() {
 	var game = new AdventureGame();
 });
+//map numbers
+// 0- free space
+// 1- 
+// 2- 
+// 3- 
+// 4- 
+// 5- 
+// 6- unstoppable wall
 
 class AdventureGame {
 	constructor() {
+		//setup variables
+		this.adventureText = [];
+		this.position = [0,0];
+
+		this.generateMap();
 		this.getElements();
 		this.assignListeners();
 		this.introduction();
+		this.displayText();
+	}
+
+	generateMap() {
+		this.map = [
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+		]
 	}
 
 	getElements() {
@@ -17,20 +52,30 @@ class AdventureGame {
 
 	assignListeners() {
 		this.button.addEventListener("click", function(event) {
-			console.log(event);
-		});
-
-		this.textOutput.addEventListener("click", function(event) {
-			console.log(event);
-		});
-
-		this.minimap.addEventListener("click", function(event) {
-			console.log(event);
+			gameLogic();
 		});
 	}
 
+	gameLogic() {
+
+	}
+
 	introduction() {
-		console.log("Welcome to the Text Adventure!");
-	}	
+		this.addText("You awake from a slumber in the middle of the woods.");
+	}
+
+	addText(text) {
+		this.adventureText.push(text);
+	}
+
+	displayText() {
+		//this.textOutput.innerText = "WEVE GOT TEXT\nhelo";
+		var buildAString = "";
+		for (var i = this.adventureText.length - 1; i >= 0; i--) {
+			buildAString += this.adventureText[i] + "\n";
+			//buildAString += "\n";
+		}
+		this.textOutput.innerText = buildAString;
+	}
 }
 
